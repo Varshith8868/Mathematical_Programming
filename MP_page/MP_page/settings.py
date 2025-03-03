e165cb1d4a84301976a9e1684b3703e8914a37b7
Varshith8868 <konalavasrshith@gmail.com>
2025-03-03 15:34:02 +0530

    hnjm

--- MP_page/MP_page/settings.py
+++ MP_page/MP_page/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-528x80wsjqalz%$sbal)wes1s2t^ly51n47_7$v8^ypm0kbzeh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
